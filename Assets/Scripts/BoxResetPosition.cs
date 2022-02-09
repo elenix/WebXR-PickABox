55b26ead1931f3e4e0b0a23da5121738fa0493d5
MaGICX 03 <magicxvislab@gmail.com>
2022-02-09 11:49:56 +0800

    add new files

---
+++ Assets/Scripts/BoxResetPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxResetPosition : MonoBehaviour
+{
+    Vector3 originalPost;
+
+    private void Start()
+    {
+        originalPost = gameObject.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Base")
+        {
+            Debug.Log("something happen");
+            gameObject.transform.position = originalPost;
+        }
+    }
+}
